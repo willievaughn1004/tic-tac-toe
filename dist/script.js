e3c64555ee0c659c47f8e752eb6c94c08ee9a1ba
Joe <willievaughn1004@gmail.com>
2023-08-16 16:30:10 -0400

    Add win conditions

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-//
+
 
 const Gameboard = {
   board: [null, null, null, null, null, null, null, null, null],
@@ -62,8 +62,8 @@ function setGameLogic() {
 
 const controlGameFlow = () => {
   const controller = setGameLogic();
-
   const squares = document.querySelectorAll(".square");
+  const playerTurn = document.querySelector(".player-turn");
 
   const updateBoardVisual = (board) => {
     for (i = 0; i < board.length; i++) {
@@ -83,22 +83,70 @@ const controlGameFlow = () => {
     }
   };
 
-  //   const setWinConditions = () => {
-  //     const currentGameBoard = Gameboard.getBoard();
+  const setWinConditions = () => {
 
-  //     if (Game) {
-  //     }
-  //   };
+    if (
+      playerTurn.textContent === "Player O wins." ||
+      playerTurn.textContent === "Player X wins."
+    ) {
+      return;
+    }
+
+    const currentBoard = Gameboard.getBoard();
+
+    const winScenarios = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const combination of winScenarios) {
+      const [a, b, c] = combination;
+
+      if (
+        currentBoard[a] === "x" &&
+        currentBoard[b] === "x" &&
+        currentBoard[c] === "x"
+      ) {
+        console.log("Player X Wins.");
+        playerTurn.textContent = "Player X wins.";
+      } else if (
+        currentBoard[a] === "o" &&
+        currentBoard[b] === "o" &&
+        currentBoard[c] === "o"
+      ) {
+        playerTurn.textContent = "Player O wins.";
+      }
+    }
+  };
 
   const restartButton = document.querySelector(".restart");
+
   const setResetButton = () => {
     Gameboard.resetBoard();
+    squares.forEach((square) => (square.innerHTML = ""));
+    console.log(controller.getActivePlayer());
+    if (controller.getActivePlayer() === 2) {
+      controller.changePlayer();
+    };
+
+    playerTurn.innerText = "Player X's Turn"
   };
 
   restartButton.addEventListener("click", setResetButton);
 
   const updatePlayerTurn = () => {
-    const playerTurn = document.querySelector(".player-turn");
+    if (
+      playerTurn.textContent === "Player O wins." ||
+      playerTurn.textContent === "Player X wins."
+    ) {
+      return;
+    }
 
     if (controller.getActivePlayer() === 1) {
       playerTurn.textContent = "Player O's Turn";
@@ -113,13 +161,14 @@ const controlGameFlow = () => {
     controller.changePlayer();
     Gameboard.displayBoard();
     updateBoardVisual(Gameboard.getBoard());
+    setWinConditions();
   };
 
   squares.forEach((square) =>
     square.addEventListener("click", function () {
       if (this.innerHTML === "") {
         const tile = this.getAttribute("id");
-        console.log(this.innerHTML)
+        console.log(this.innerHTML);
         playRound(tile);
       }
     })
